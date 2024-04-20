3d6f769fba0232dd983821d15a9a01d5cb746397
Fernando Mauricio <88916124+fmauriccio@users.noreply.github.com>
2024-04-20 11:06:48 -0300

    Update scriptlogin.js

--- scriptlogin.js
+++ scriptlogin.js
@@ -1,10 +1,28 @@
 function fazerLogin() {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+  var emailInput = document.getElementById("emailInput");
+  var passwordInput = document.getElementById("passwordInput");
+  var email = emailInput.value;
+  var password = passwordInput.value;
+  
+  if (!email.includes("@") || !email.includes(".com")) {
+    emailInput.style.outline = "2px solid red";
+    return false;
+  } else {
+    emailInput.style.outline = "";
+  }
+  
+  if (password.length < 4) {
+    passwordInput.style.outline = "2px solid red";
+    return false;
+  } else {
+    passwordInput.style.outline = "";
+  }
 
   if (email === "als@gmail.com" && password === "12345") {
     window.location.href = "home.html";
   } else {
     alert("E-mail ou senha incorretos. Por favor, tente novamente.");
   }
+
+  return false;
 }
