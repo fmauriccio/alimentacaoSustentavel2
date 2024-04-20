c50f00748b09c40f01726579d4558e8f0b24342a
Fernando Mauricio <fernandomauricio.sud@gmail.com>
2024-04-20 11:30:27 -0300

    Atualização 20/04/2024

--- scriptlogin.js
+++ scriptlogin.js
@@ -1,10 +1,28 @@
 function fazerLogin() {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  var email = email.value;
+  var password = password.value;
+  
+  if (!email.includes("@") || !email.includes(".com")) {
+    email.style.outline = "2px solid red";
+    return false;
+  } else {
+    email.style.outline = "";
+  }
+  
+  if (password.length < 4) {
+    password.style.outline = "2px solid red";
+    return false;
+  } else {
+    password.style.outline = "";
+  }
 
   if (email === "als@gmail.com" && password === "12345") {
     window.location.href = "home.html";
   } else {
     alert("E-mail ou senha incorretos. Por favor, tente novamente.");
   }
+
+  return false;
 }
